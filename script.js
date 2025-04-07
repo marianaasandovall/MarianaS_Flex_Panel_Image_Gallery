7cf12bce90100f8a3f53ac0839f9798f1b2d7a2b
unknown <mariana10sandoval@gmail.com>
2025-04-07 16:22:44 -0700

    added onclick.wav, removed inline JS script within HTML, made a script.js file and moved inline JS to script.js, fixed 'click' sound runs smoothly now.

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+const clickSound = new Audio('onclick.mp3');
+function toggleOpen() {
+  clickSound.currentTime = 0;
+  clickSound.play();
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
